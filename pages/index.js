71701eb33b0a0f9118bbbc1a5ff095c6b7096d6f
gptus <yhc9308@naver.com>
2023-09-21 21:18:14 +0900

    css 및 글로벌 연습

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
-import NavBar from "../components/NavBar";
-
 export default function Home() {
   return (
     <div>
-      <NavBar />
-      <h1>Hello</h1>
+      <h1 className="active">Hello</h1>
     </div>
   );
 }
